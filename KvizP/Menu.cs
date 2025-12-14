5b19c13dd04ec730944c6149a75e5abc702679fd
Emina <Emina@DESKTOP-TKRH4V2>
2025-12-14 18:30:47 +0100

    Add project files.

---
+++ KvizP/Menu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KvizP
+{
+    internal class Menu
+    {
+        public static int Odabir()
+        {
+            Console.WriteLine("KVIZ ZNANJA");
+            Console.WriteLine("1. Singleplayer");
+            Console.WriteLine("2. Multiplayer");
+            while (true)
+            {
+                try
+                {
+                    int input = int.Parse(Console.ReadLine());
+                    if (input==1||input==2)
+                    {
+                        return input;
+                    }
+                    else
+                        Console.WriteLine("Unesite 1 ili 2.");
+                }
+                catch
+                {
+                    Console.WriteLine("Pogrešan unos.");
+                }
+            }
+        }
+    }
+
+}
+
---
+++ KvizP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+    namespace KvizP
+{
+internal class Program
+    {
+        static void Main()
+        {
+            int izbor = Menu.Odabir();
+            if (izbor == 1)
+            {
+                Console.WriteLine("Singleplayer mode");
+            }
+            else
+                Console.WriteLine("Multiplayer mode");
+            Console.ReadKey();
+        }
+    }
+}
+
